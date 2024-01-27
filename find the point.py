d1ad0e459a67cc26c6b89b0b4db78863569fba95
ArockiyaJeyaStefy <143931429+ArockiyaJeyaStefy@users.noreply.github.com>
2024-01-27 12:26:42 +0530

    Add files via upload

---
+++ find the point.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'findPoint' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER px
+#  2. INTEGER py
+#  3. INTEGER qx
+#  4. INTEGER qy
+#
+
+def findPoint(px, py, qx, qy):
+    p2q_x = qx - px
+    p2q_y = qy - py
+    r_pt = [qx + p2q_x, qy + p2q_y]
+    return r_pt
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    for n_itr in range(n):
+        first_multiple_input = input().rstrip().split()
+
+        px = int(first_multiple_input[0])
+
+        py = int(first_multiple_input[1])
+
+        qx = int(first_multiple_input[2])
+
+        qy = int(first_multiple_input[3])
+
+        result = findPoint(px, py, qx, qy)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
